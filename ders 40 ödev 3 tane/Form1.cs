d5f856f9d994cd086cb2419b04cc14ca4b81b1de
alican <doc.alican@hotmail.com>
2019-02-15 15:30:56 +0300

    4 e bolunen sayiları gostorme foreach

---
+++ ders 40 ödev 3 tane/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ders_40_ödev_3_tane
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+       
+                {
+                    int sayac = 0;
+                    int toplam = 0;
+                    int[] sayilar = { 10, 20, 5, 12, 32 };
+                    foreach (int k in sayilar)
+                    {
+                        listBox1.Items.Add(k);
+                        if (k % 4 == 0)
+                {
+                    listBox2.Items.Add(k);
+                    toplam = toplam + k;
+                    sayac++;
+                }
+                            
+
+                    }
+                    label1.Text = sayac.ToString();
+                    label2.Text = toplam.ToString();
+
+
+                }
+        }
+    }
+
---
+++ ders 40 ödev 3 tane/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ders_40_ödev_3_tane
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
